89984f41628ceb6f2664afd5b4af1c5ebad87904
Samuel Mutemi <mtsammy40@hotmail.com>
2019-02-20 02:13:46 +0300

    first commit

---
+++ routes/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/sendMail', function(req, res, next) {
+  var emailto = req.param('email');
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'mtsammy40@gmail.com',
+      pass: 'aweSAM40'
+    }
+  });
+  
+  var mailOptions = {
+    from: 'mtsammy40@gmail.com',
+    to: 'mtsammy40@hotmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+      res.send('There was an error. Please check data');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.send('Email sent');
+    }
+  });
+});
+
+module.exports = router;
