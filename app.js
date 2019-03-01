14aaa79ab2c2ff5fc30a252e06c55eecf8a93064
Samuel Mutemi <mtsammy40@gmail.com>
2019-03-01 12:18:50 +0300

    Enable CORS

--- app.js
+++ app.js
@@ -19,9 +19,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+//enable CORS
+app.use(function(req, res, next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
